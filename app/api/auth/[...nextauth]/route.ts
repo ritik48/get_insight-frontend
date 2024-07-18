5706e31a55b9c11935ae70dcf61c0c9fc9436501
Ritik Raj <raj769417@gmail.com>
2024-07-18 17:59:34 +0530

    refactor: removed authoptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,7 @@
+import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth({
-    providers: [
-        // @ts-ignore
-        CredentialsProvider({
-            name: "Credentials",
-            async authorize(credentials: any) {
-                console.log("credential = ", credentials);
-
-                const user = {
-                    id: "1",
-                    name: "J Smith",
-                    email: "jsmith@example.com",
-                };
-
-                if (user) {
-                    return user;
-                } else {
-                    return null;
-                }
-            },
-        }),
-    ],
-    pages: {
-        signIn: "/signin",
-    },
-});
+const handler = NextAuth(authOptions);
 
 export const GET = handler;
 export const POST = handler;
