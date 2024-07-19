daddc4d46a254c66df35fdf9f22795113d950306
Ritik Raj <raj769417@gmail.com>
2024-07-20 03:37:43 +0530

    feat: used react-form, zod

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,11 +1,40 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const userSchema = z.object({
+    username: z.string().min(3, "Username must contain at least 3 character"),
+    password: z.string().min(3, "Password must contain at least 3 character"),
+});
 
 export default function Signin() {
+    const form = useForm<z.infer<typeof userSchema>>({
+        resolver: zodResolver(userSchema),
+    });
+
+    const isSubmitting = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof userSchema>) => {
+        const res = await signIn("credentials", {
+            ...values,
+            callbackUrl: "/home",
+        });
+        console.log(res);
+    };
+
     return (
         <section className="flex-1 flex flex-col">
             <div className="container flex-grow">
@@ -16,28 +45,55 @@ export default function Signin() {
                         </h2>
                         <p>Enter your username and password</p>
                     </div>
-                    <div className="w-full flex flex-col gap-2">
-                        <Input placeholder="Username" />
-                        <Input placeholder="Password" type="password" />
-                        <Button
-                            className="mt-3"
-                            onClick={() =>
-                                signIn("credentials", {
-                                    username: "ritik48",
-                                    password: "12458",
-                                    callbackUrl: "/home",
-                                })
-                            }
-                        >
-                            Login
-                        </Button>
-                        <Link
-                            href={"/signup"}
-                            className="text-left text-sm hover:underline mt-1"
-                        >
-                            Don&apos;t have an account ?
-                        </Link>
-                    </div>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <div className="w-full flex flex-col gap-2">
+                                <FormField
+                                    name="username"
+                                    control={form.control}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Username"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage className="text-xs text-red-500" />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    name="password"
+                                    control={form.control}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Password"
+                                                    {...field}
+                                                    type="password"
+                                                />
+                                            </FormControl>
+                                            <FormMessage className="text-sm text-red-500" />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button
+                                    disabled={isSubmitting}
+                                    className="mt-3"
+                                >
+                                    {isSubmitting ? "Logging in" : "Login"}
+                                </Button>
+                                <Link
+                                    href={"/signup"}
+                                    className="text-left text-sm hover:underline mt-1"
+                                >
+                                    Don&apos;t have an account ?
+                                </Link>
+                            </div>
+                        </form>
+                    </Form>
                 </div>
             </div>
         </section>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,54 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
+
+const userSchema = z.object({
+    username: z.string().min(3, "Username must contain at least 3 character"),
+    password: z.string().min(3, "Password must contain at least 3 character"),
+});
 
 export default function Signup() {
+    const form = useForm<z.infer<typeof userSchema>>({
+        resolver: zodResolver(userSchema),
+    });
+
+    const isSubmitting = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof userSchema>) => {
+        try {
+            const res = await fetch("/api/signup", {
+                method: "POST",
+                body: JSON.stringify(values),
+            });
+
+            const data = await res.json();
+
+            if (!data.success) {
+                toast.error(data.message || "Something went wrong");
+                return;
+            }
+            toast.success("Account created successfull");
+
+            await signIn("credentials", {
+                ...values,
+                callbackUrl: "/home",
+            });
+        } catch (error) {}
+    };
     return (
         <section className="flex-1 flex flex-col">
             <div className="container flex-grow">
@@ -13,17 +57,55 @@ export default function Signup() {
                         <h2 className="text-xl font-bold">Create an account</h2>
                         <p>Enter your username and password</p>
                     </div>
-                    <div className="w-full flex flex-col gap-2">
-                        <Input placeholder="Username" />
-                        <Input placeholder="Password" type="password" />
-                        <Button className="mt-3">Signup</Button>
-                        <Link
-                            href={"/signin"}
-                            className="text-left text-sm hover:underline mt-1"
-                        >
-                            Already have an account ?
-                        </Link>
-                    </div>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <div className="w-full flex flex-col gap-2">
+                                <FormField
+                                    name="username"
+                                    control={form.control}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Username"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage className="text-xs text-red-500" />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    name="password"
+                                    control={form.control}
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Password"
+                                                    {...field}
+                                                    type="password"
+                                                />
+                                            </FormControl>
+                                            <FormMessage className="text-sm text-red-500" />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button
+                                    disabled={isSubmitting}
+                                    className="mt-3"
+                                >
+                                    {isSubmitting ? "Creating..." : "Signup"}
+                                </Button>
+                                <Link
+                                    href={"/signin"}
+                                    className="text-left text-sm hover:underline mt-1"
+                                >
+                                    Already have an account ?
+                                </Link>
+                            </div>
+                        </form>
+                    </Form>
                 </div>
             </div>
         </section>
