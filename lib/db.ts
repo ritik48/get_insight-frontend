0753ebe785b7753b9096fe1a7216333b8dab19f3
Ritik Raj <raj769417@gmail.com>
2024-07-20 04:28:37 +0530

    feat: access db url from env

--- lib/db.ts
+++ lib/db.ts
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 
 const connection: { isConnected?: number } = {};
 
+const MONGODB_URL =
+    process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/next-app";
+
 const connectDb = async () => {
     if (connection.isConnected) {
         return;
     }
 
-    const db = await mongoose.connect("mongodb://127.0.0.1:27017/next-app");
+    const db = await mongoose.connect(MONGODB_URL);
+    console.log("Connected ===========================================");
     connection.isConnected = db.connections[0].readyState;
 };
 
