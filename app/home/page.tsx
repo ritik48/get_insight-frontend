cec073b5a2935fe7601450ab551991949e4b5d58
Ritik Raj <raj769417@gmail.com>
2024-07-19 15:04:53 +0530

    feat: added shadcn component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import { authOptions } from "@/lib/authOptions";
-import { getServerSession, Session } from "next-auth";
+import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { Result } from "./components/result";
+import { DataForm } from "./components/DataForm";
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
@@ -11,7 +15,14 @@ export default async function Home() {
     return (
         <section className="flex-1 flex flex-col">
             <div className="container flex-grow flex justify-center">
-                <div className="text-5xl mt-40">Welcome</div>
+                <div className="my-16 w-full flex items-start gap-3">
+                    <DataForm />
+                    <Result
+                        summary="weffew"
+                        sentiment="positive"
+                        keywords={["abdc", "mklo"]}
+                    />
+                </div>
             </div>
         </section>
     );
