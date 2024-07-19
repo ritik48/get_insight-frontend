77f48b3e557de2f6fb33b53c765efe675f4992dc
Ritik Raj <raj769417@gmail.com>
2024-07-19 15:08:34 +0530

    feat: added form

--- app/home/components/DataForm.tsx
+++ app/home/components/DataForm.tsx
@@ -1,16 +1,62 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+    text: z.string().min(40, "Text must be atleast 40 character"),
+});
+
 export function DataForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            text: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log("values = ", values);
+    };
     return (
-        <div className="flex-1 flex flex-col gap-3 items-start h-[60%]">
-            <h3 className="text-xl font-semibold">
-                Write text or upload file to analyse
-            </h3>
-            <div className="w-full h-full space-y-2">
-                <Textarea className="h-full transition-all duration-300 ease-in-out" />
-                <Button>Upload</Button>
-            </div>
-        </div>
+        <Form {...form}>
+            <form
+                className="h-[250px] sm:h-full sm:flex-1 flex flex-col w-full sm:w-auto"
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                <FormField
+                    control={form.control}
+                    name="text"
+                    render={({ field }) => (
+                        <FormItem className="h-full sm:h-[60%] flex flex-col gap-1">
+                            <FormLabel className="text-lg sm:text-xl font-semibold text-foreground">
+                                Write text or upload file to analyse
+                            </FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    {...field}
+                                    className="transition-all w-full h-full duration-300 ease-in-out"
+                                />
+                            </FormControl>
+                            <FormMessage className="text-xs sm:text-sm text-red-500" />
+                        </FormItem>
+                    )}
+                />
+
+                <Button className="mt-2 w-fit">Upload</Button>
+            </form>
+        </Form>
     );
 }
