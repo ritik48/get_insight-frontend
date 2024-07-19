c7b41f8a7ccc4de32bc8417963aa15e3c828bcb6
Ritik Raj <raj769417@gmail.com>
2024-07-20 02:29:55 +0530

    refactor: moved formscham validation into differrent file

--- app/home/components/DataForm.tsx
+++ app/home/components/DataForm.tsx
@@ -12,29 +12,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
+import { formSchema } from "@/lib/formSchemaValidation";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
-const formSchema = z.object({
-    text: z
-        .string()
-        .refine((value) => {
-            if (!value) return true;
-            return value.length >= 20;
-        }, "Text should have at least 20 charcters")
-        .optional(),
-    file_data: z
-        .any()
-        .refine((files) => {
-            if (!files || files.length === 0) return true;
-            return files[0].size <= 1024 * 1024;
-        }, "File size must be 1mb or less")
-        .optional(),
-    type_of_data: z.enum(["text", "file"], { message: "Invalid type of data" }),
-});
 
 export function DataForm({
     setResult,
