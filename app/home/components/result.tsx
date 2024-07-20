0d3b9e3237ec52dfb4df426d0d9e817459506d70
Ritik Raj <raj769417@gmail.com>
2024-07-20 23:41:53 +0530

    refactor: style change

--- app/home/components/result.tsx
+++ app/home/components/result.tsx
@@ -3,39 +3,41 @@ import { Textarea } from "@/components/ui/textarea";
 type ResultProps = {
     summary?: string;
     sentiment?: string;
-    keywords?: string;
+    keywords?: string[];
 };
 
 const keywords = ["country", "jobs", "population", "inflation"];
 
 export function Result({ summary, sentiment, keywords }: ResultProps) {
-    const keywords_list = keywords?.split("\n").map((k) => {
-        const parts = k.trim().split("-");
-        return parts.length > 1 && parts[0] === "" ? parts[1].trim() : k.trim();
-    });
-
+    const senti = sentiment?.toLowerCase();
     return (
-        <div className="flex-1 flex flex-col sm:h-full gap-5 w-full sm:w-auto">
-            <div className="h-[200px] sm:h-[60%] flex flex-col gap-3">
+        <div className="flex flex-col gap-5">
+            <div className="max-h-[400px] flex flex-col gap-3">
                 <h3 className="text-xl font-semibold">Summary</h3>
-                <div className="w-full h-full">
-                    <Textarea
-                        className="h-full"
-                        readOnly={true}
-                        value={summary}
-                    />
-                </div>
-            </div>
 
+                <div className="border rounded-md p-4">{summary}</div>
+            </div>
             <div className="flex flex-col gap-5">
                 <div>
                     <h3 className="font-semibold">Sentiment</h3>
-                    <div>{sentiment}</div>
+                    <div
+                        className={`${
+                            senti === "positive"
+                                ? "bg-green-500"
+                                : senti === "negative"
+                                ? "bg-red-500"
+                                : senti === "neutral"
+                                ? "bg-yellow-500"
+                                : "bg-none"
+                        } w-fit px-3 py-1 rounded-md text-zinc-100 font-semibold`}
+                    >
+                        {sentiment}
+                    </div>
                 </div>
                 <div className="space-y-2">
                     <h3 className="font-semibold">Keywords</h3>
                     <div className="font-semibold flex gap-3 flex-wrap">
-                        {keywords_list?.map((k, index) => (
+                        {keywords?.map((k: string, index: number) => (
                             <span
                                 key={index}
                                 className="bg-zinc-800 text-zinc-100 rounded-md px-2 py-1 text-sm"
