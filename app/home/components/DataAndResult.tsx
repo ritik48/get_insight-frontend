edefd55f4dbb3450b616011e9b96bf9fb701390e
Ritik Raj <raj769417@gmail.com>
2024-07-20 23:44:12 +0530

    refactor: modified the propt type

--- app/home/components/DataAndResult.tsx
+++ app/home/components/DataAndResult.tsx
@@ -8,13 +8,13 @@ export function DataAndResult() {
     const [result, setResult] = useState<{
         summary?: string;
         sentiment?: string;
-        keywords?: string;
+        keywords?: string[];
     }>();
 
     return (
         <section className="h-full">
             <div className="container flex flex-col h-full">
-                <div className="my-16 flex-grow flex flex-col sm:flex-row items-start gap-6">
+                <div className="my-16 sm:w-[60%] w-full flex-grow flex flex-col gap-6 mx-auto">
                     <DataForm setResult={setResult} />
                     <Result
                         summary={result?.summary}
