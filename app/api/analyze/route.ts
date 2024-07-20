1862e02d803f3369ffd4251237043d45b03479de
Ritik Raj <raj769417@gmail.com>
2024-07-20 22:00:10 +0530

    feat: save the response to databse

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -1,10 +1,13 @@
+import { Data } from "@/app/_models/data";
+import { connectDb } from "@/lib/db";
 import { handleAuth } from "@/lib/middleware";
 
-const BACKEND_URL = process.env.BACKEND_URL;
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:4000";
 
 export async function POST(req: Request, res: Response) {
     try {
-        await handleAuth();
+        const username = await handleAuth();
+        await connectDb();
 
         const formData = await req.formData();
 
@@ -14,7 +17,28 @@ export async function POST(req: Request, res: Response) {
         });
 
         const result = await response.json();
-        console.log(result);
+
+        const keywords_list = result?.data?.keywords
+            ?.split("\n")
+            .map((k: string) => {
+                const parts = k.trim().split("-");
+                return parts.length > 1 && parts[0] === ""
+                    ? parts[1].trim()
+                    : k.trim();
+            });
+
+        const data_to_save = {
+            summary: result.data.summary,
+            sentiment: result.data.sentiment.toLowerCase(),
+            keywords: keywords_list,
+            username: username,
+            url: result?.upload_result?.url,
+            filename: result?.upload_result?.filename,
+            data_type: formData.get("type_of_data"),
+            text: formData.get("text"),
+        };
+
+        await Data.create({ ...data_to_save });
 
         return Response.json({ ...result }, { status: 200 });
     } catch (error) {
