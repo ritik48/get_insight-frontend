aa1eccda71f3019939af84ce17aa8bc5141fa120
Ritik Raj <raj769417@gmail.com>
2024-07-20 22:04:25 +0530

    feat: fetch all the data analysis done by the user for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { DataHistory } from "./components/DataHistory";
+import { getUserHistory } from "@/lib/services/getUserHistory";
 
 export default async function Dashboard() {
     const session = await getServerSession(authOptions);
@@ -10,11 +11,13 @@ export default async function Dashboard() {
         redirect("/");
     }
 
+    const userDataHistory = await getUserHistory(session.user.username);
+
     return (
         <section className="mt-5">
             <div className="container space-y-2">
                 <h1 className="text-xl font-bold">Dashboard</h1>
-                <DataHistory />
+                <DataHistory userData={userDataHistory.data} />
             </div>
         </section>
     );
