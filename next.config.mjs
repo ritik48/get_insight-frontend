0e14b9210f3582bef79e648e44f148430bdd0238
Ritik <raj769417@gmail.com>
2025-09-05 19:31:55 +0530

    text img

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https", // This was missing!
+        hostname: "d3cqgx0nfm61lc.cloudfront.net",
+        port: "", // Add this explicitly
+        pathname: "/**", // Add this to allow all paths
+      },
+    ],
+  },
+};
 
 export default nextConfig;
